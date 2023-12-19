607101d3121d9573509c6e72e73ac8e4a543ed96
Andrei Dochkin <andrey.dochkin@gmail.com>
2023-12-20 00:31:15 +0400

    fix: frontend

--- backend/errors/UnhandledError.js
+++
@@ -1,8 +0,0 @@
-class UnhandledError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'UnhandledError';
-    this.statusCode = 500;
-  }
-}
-module.exports = UnhandledError;
--- backend/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.statusCode);
-  console.error(err.message);
-  const { statusCode = 500, message = 'На сервере произошла ошибка' } = err;
-  res.status(statusCode).send({ message });
-  next();
-};
-
-module.exports = errorHandler;
